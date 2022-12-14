19ae47ed229ae6bbee4a4b420eb33aeed9b6edcb
Mr Tea Bag <107035783+Mr-Tea-Bag@users.noreply.github.com>
2022-12-14 13:47:50 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,6 @@
+var superheroes = require("superheroes");
+var supervillain = require("supervillains");
+var MySuperHeroName = superheroes.random();
+var MySuperVillainName = supervillain.random();
+console.log(MySuperHeroName);
+console.log(MySuperVillainName);
